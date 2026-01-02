0ad87d6d3b2bdeea1ee40b488e873c0c3c7aa2c3
Sandesh <sandeshchy2059@gmail.com>
2026-01-02 21:19:22 +0545

    return book method is added

--- library_management/services/library_services.py
+++ library_management/services/library_services.py
@@ -10,3 +10,10 @@ class library_services():
                 else:
                     return f"{title} is assigned to someone else"
         return f"sorry {title} is not available"
+    
+    def student_return_book(self , title):
+        for book in books:
+            if book.title == title:
+                book.return_book()
+                return f"{title} has been returned successfully"
+        return f"{title} is not the property of library"
