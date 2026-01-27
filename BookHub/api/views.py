513a5d84ac972f95d03e152390348a352fb97750
Sandesh <sandeshchy2059@gmail.com>
2026-01-27 11:50:19 +0545

    view for BookDetail is created

--- BookHub/api/views.py
+++ BookHub/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from books.models import Book
 from .serializers import BookSerializer
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.views import APIView, Http404
 from rest_framework import status
 
 class Books(APIView):
@@ -11,3 +11,17 @@ class Books(APIView):
         books = Book.objects.all()
         serializer = BookSerializer(books, many = True)
         return Response(serializer.data)
+
+class BookDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404
+        
+    def get(self,request, pk):
+        books = self.get_object(pk=pk)
+        serializer = BookSerializer(books)
+        return Response(serializer.data)
+
